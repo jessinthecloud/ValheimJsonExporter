0ab2674b2a73d7346726b91c0d88740d4185c2f1
Jess <code@jessinthe.cloud>
2021-05-19 19:13:03 -0400

    failed attempt at removing unicode control chars

--- ValheimJsonExporter/ValheimJsonExporter.cs
+++ ValheimJsonExporter/ValheimJsonExporter.cs
@@ -57,6 +57,48 @@ namespace ValheimJsonExporter
             string text = Localization.instance.Localize(key).Replace("\n", "<br/>");
             // strip non-ascii
             text = Regex.Replace(text, @"[^\u0000-\u007F]+", string.Empty);
+           /* 
+            * none of this is working
+            // remove invalid hex
+            // not working
+            text = Regex.Replace(text, @"(?i)[\x00-\x1F\x7F]+", string.Empty);
+            // not working
+            text = Regex.Replace(text, @"(?i)[\u0000-\u001F\u007F]+", string.Empty);
+
+            // YOU MADE ME DO THIS, C#
+            text = text.Replace('\u0000', ' ')
+        .Replace('\u0001', ' ')
+        .Replace('\u0002', ' ')
+        .Replace('\u0003', ' ')
+        .Replace('\u0004', ' ')
+        .Replace('\u0005', ' ')
+        .Replace('\u0006', ' ')
+        .Replace('\u0007', ' ')
+        .Replace('\u0008', ' ')
+        .Replace('\u0009', ' ')
+        .Replace('\u000a', ' ')
+        .Replace('\u000b', ' ')
+        .Replace('\u000c', ' ')
+        .Replace('\u000d', ' ')
+        .Replace('\u000e', ' ')
+        .Replace('\u000f', ' ')
+        .Replace('\u0010', ' ')
+        .Replace('\u0011', ' ')
+        .Replace('\u0012', ' ')
+        .Replace('\u0013', ' ')
+        .Replace('\u0014', ' ')
+        .Replace('\u0015', ' ')
+        .Replace('\u0016', ' ')
+        .Replace('\u0017', ' ')
+        .Replace('\u0018', ' ')
+        .Replace('\u0019', ' ')
+        .Replace('\u001a', ' ')
+        .Replace('\u001b', ' ')
+        .Replace('\u001c', ' ')
+        .Replace('\u001d', ' ')
+        .Replace('\u001e', ' ')
+        .Replace('\u001f', ' ');*/
+
             // escape for JS
             // text = HttpUtility.JavaScriptStringEncode(text); // is just blank, BepinEx needs System.Web.dll
 
